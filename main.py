2f28e0e3d2dea2b3c348d824b0b073646722f3a7
John Yuen <johnyuen@Johns-MacBook-Pro.local>
2021-12-16 13:12:19 -0500

    something

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+from bot import Player
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='-', intents=intents)
+
+@bot.event
+async def on_ready():
+	print(f"{bot.user.name} is ready")
+
+async def setup():
+	await bot.wait_until_ready()
+	bot.add_cog(Player(bot))
+
+bot.loop.create_task(setup())
+bot.run("OTA2MDI2MTAwNDc0MzkyNTk3.YYSojw.B8sJe1LBySyAaueKTJs6nBpBCfI")
